063d58eaafd652e442510fcc5abc162ddc97253c
AntoineRenou <antoinerenou95@gmail.com>
2016-11-05 15:01:18 +0100

    Find Intruder V0.0.4 - Antoine RENOU
    Avancement du projet :
    - Ajout des sons
    
    NB : Problème de fonctionnement sur Lollipop

--- MainActivity.java
+++ MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         setContentView(R.layout.activity_main);
-
+//
         Resources res = getResources();
         Drawable drawable = res.getDrawable(R.drawable.spaceinvader1);
         Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
